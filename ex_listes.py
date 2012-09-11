3b42e8ad2a074e74d9a172b7e2d2b8b31e7911bc
Laurent Claessens <moky.math@gmail.com>
2012-09-11 19:09:48 +0200

    Un peu de python

---
+++ ex_listes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8 -*-
+
+A=[1,3,6,15]    # liste de nombres
+B=["bonjour","au revoir"] # liste de chaînes de caractères.
+
+print(A)
+print(B)
---
+++ exemples_python.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Ce fichier crée les fichiers de réponse des exemples de python pour l'AP.
+
+python3 ex_listes.py > res_ex_listes.txt
