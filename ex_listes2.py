24445b622553a42488acf43b599299962cdb67b9
Laurent Claessens <moky.math@gmail.com>
2012-09-12 18:48:52 +0200

    python

---
+++ ex_listes2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+
+A=[1,3,6,15] 
+B=["bonjour","au revoir","salut"] 
+
+A.append(15)
+A.append("un mot")      # Un liste peut mélanger des éléments de différents types
+
+print(A)
+
+B.append("Ceci est le dernier")
+print(B[1])
+print(B[-1])
+
--- exemples_python.sh
+++ exemples_python.sh
@@ -3,3 +3,4 @@
 # Ce fichier crée les fichiers de réponse des exemples de python pour l'AP.
 
 python3 ex_listes.py > res_ex_listes.txt
+python3 ex_listes2.py > res_ex_listes2.txt
