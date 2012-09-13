ca1becc39723091779b2d6e8332121446882ebab
Laurent Claessens <moky.math@gmail.com>
2012-09-13 11:28:16 +0200

    mise en ordre

---
+++ chaussures.py
@@ -0,0 +1,26 @@
+print("Bonjour !")
+
+liste=[]                # liste vide
+liste.extend(3*[40])    # 3*[40] est la liste [40,40,40]
+liste.extend(5*[41])
+liste.extend(10*[42])
+liste.extend(8*[43])
+liste.extend(2*[44])
+liste.extend(4*[45])
+
+print(liste)           # Afficher liste
+        
+
+#   [40,40,40,41,41,41,41,41,42,42,42,42,42,42,42,42,42,42,43,
+#   43,43,43,43,43,43,43,44,44,45,45,45,45]
+
+n=len(liste)        # len(liste) est la longueur de la liste
+somme=sum(liste)    # la somme de la liste
+
+print(n)        # 32      
+print(somme)    # 1357
+
+moyenne=somme/n     # diviser la somme la la longueur donne la moyenne
+
+print(moyenne)  # 42.40625
+
--- exemples_python.sh
+++ exemples_python.sh
@@ -4,3 +4,4 @@
 
 python3 ex_listes.py > res_ex_listes.txt
 python3 ex_listes2.py > res_ex_listes2.txt
+python3 chaussures.py > res_chaussures.txt
