f57b8a46f335e5d7d465a4ed4f50aacf3cb39240
Laurent Claessens <moky.math@gmail.com>
2012-10-09 12:24:10 +0200

    Quelque fichiers manquants.

---
+++ ex_algo.py
@@ -0,0 +1,7 @@
+def mafonction(x):
+    a=x+1
+    b=a/2
+    c=b-4
+    return c
+
+print(mafonction(3))
--- phystricksParaboleResumeHNiyfR.py
+++ phystricksParaboleResumeHNiyfR.py
@@ -3,25 +3,30 @@ from __future__ import division
 from phystricks import *
 def ParaboleResumeHNiyfR():
     pspict,fig = SinglePicture("ParaboleResumeHNiyfR")
-    pspict.dilatation(1)
+    pspict.dilatation(0.7)
 
     x=var('x')
-    x1=1
-    x2=3
-    f(x)=phyFunction((x-x1)*(x-x2)).graph(-1,4)
+    mx=0.5
+    Mx=4.5
+    x1=1.5
+    x2=3.5
+    f=phyFunction((x-x1)*(x-x2)).graph(mx,Mx)
     s=(x1+x2)/2
     S=Point( s,f(s) )
     X0=Point(s,0)
     X1=Point(x1,0)
     X2=Point(x2,0)
-    symetrie=Segment(Point(s,-3),Point(s,3))
+    symetrie=Segment(Point(s,f(s)-1),Point(s,f(Mx)+0.5))
     symetrie.parameters.color="red"
-    X0.put_mark(0.2,90,"\( x_0\)",automatic_place=pspict)
-    X1.put_mark(0.2,90,"\( x_1\)",automatic_place=pspict)
-    X2.put_mark(0.2,90,"\( x_1\)",automatic_place=pspict)
+    X0.put_mark(0.2,45,"\( x_0\)",automatic_place=pspict)
+    X1.put_mark(0.2,225,"\( x_1\)",automatic_place=pspict)
+    X2.put_mark(0.2,-45,"\( x_2\)",automatic_place=pspict)
+    S.put_mark(0.2,-45,"\( S\)",automatic_place=pspict)
 
-    pspict.DrawGraphs(f,X1,X2,X0,S)
+    pspict.DrawGraphs(f,symetrie,X1,X2,X0,S)
+    pspict.axes.no_graduation()
     pspict.DrawDefaultAxes()
+    fig.no_figure()
     fig.conclude()
     fig.write_the_file()
 
