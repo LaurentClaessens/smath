8c23bbf38f634dc364d9a59558f84a616da445e5
Laurent Claessens <moky.math@gmail.com>
2012-10-16 05:40:41 +0200

    Correction de binomiale

--- ex_binomiale3.py
+++ ex_binomiale3.py
@@ -1,8 +1,9 @@
 from scipy import stats
 
-for N in range(4,12):
+for N in range(4,10):
     print("Questionnaire à ",N," questions")
     X=stats.binom(N,0.25)
     for k in range(0,N+1):
-        print("P(X<",k,")=",X.cdf(k))
+        print("P(X<=",k,")=",X.cdf(k))
+
 
