6a7e8879c51c47eae09ead3c89cb8dd636099bce
Laurent Claessens <moky.math@gmail.com>
2012-11-20 12:44:14 +0100

    des exos.

---
+++ ex_algo10.py
@@ -0,0 +1,9 @@
+def f(x):
+    a=3*x
+    if a>10 :
+        print("gagné")
+    if a<=10:
+        print("perdu")
+
+f(2)
+f(7)
---
+++ ex_algo11.py
@@ -0,0 +1,12 @@
+def f(x):
+    if x>=0:
+        return 1
+    if x<0:
+        return -1
+
+k=f(-12)
+l=f(200)
+m=f(0.1)
+print(k)
+print(l)
+print(m)
