ee49429bb518b0d483efb66e80cc09b13d5914ea
Laurent Claessens <moky.math@gmail.com>
2012-11-24 10:28:06 +0100

    new file:   phystricksfigureNNgEEzx.py

---
+++ phystricksfigureNNgEEzx.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def figureNNgEEzx():
+    pspict,fig = SinglePicture("figureNNgEEzx")
+    pspict.dilatation(1)
+
+    A=Point(3,2)
+    B=Point(1,3)
+    O=Point(0,0)
+    v=AffineVector(A,B)
+    M=Point(0,0)+v
+    w=AffineVector(O,M)
+
+    A.put_mark(0.2,0,"\( A\)",automatic_place=pspict)
+    B.put_mark(0.2,90,"\( B\)",automatic_place=pspict)
+    M.put_mark(0.2,135,"\( M\)",automatic_place=pspict)
+    O.put_mark(0.2,45,"\( O\)",automatic_place=pspict)
+    A.parameters.symbol="x"
+    B.parameters.symbol="x"
+    M.parameters.symbol="x"
+
+    pspict.DrawGraphs(A,B,v,M,w,O)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
