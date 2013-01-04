cefbaa0cf7f01aa1a129bba34292682f45b0edca
Laurent Claessens <moky.math@gmail.com>
2013-01-04 22:47:40 +0100

    Exo diagramme en boites.

--- phystricksfigureEHyIMRQ.py
+++ phystricksfigureEHyIMRQ.py
@@ -1,13 +1,31 @@
 # -*- coding: utf8 -*-
 from phystricks import *
+
 def figureEHyIMRQ():
     pspict,fig = SinglePicture("figureEHyIMRQ")
-    pspict.dilatation(1)
+    pspict.dilatation_X(0.003)
 
-    x=var('x')
-    <+Définition des objets+>
+    pspict.Mx_acceptable_BB=5000
+    pspict.mx_acceptable_BB=-400
 
-    pspict.DrawGraphs(<++>)
+    moustache1=Moustache(500,1000,1400,1500,2300,h=0.5,delta_y=1)
+    moustache2=Moustache(300,950,1500,3200,4000,h=0.5,delta_y=2)
+    moustache3=Moustache(700,800,950,1800,3000,h=0.5,delta_y=3)
+    moustache4=Moustache(1000,1600,1900,2300,2500,h=0.5,delta_y=4)
+    moustache5=Moustache(1300,1700,1900,2050,2200,h=0.5,delta_y=5)
+
+    moustache1.put_mark(0.3,0,"\( A\)",automatic_place=pspict)
+    moustache2.put_mark(0.3,0,"\( B\)",automatic_place=pspict)
+    moustache3.put_mark(0.3,0,"\( C\)",automatic_place=pspict)
+    moustache4.put_mark(0.3,0,"\( D\)",automatic_place=pspict)
+    moustache5.put_mark(0.3,0,"\( E\)",automatic_place=pspict)
+
+    pspict.DrawGraphs(moustache1,moustache2,moustache3,moustache4,moustache5)
+    pspict.axes.single_axeX.Dx=500
+    pspict.axes.single_axeY.no_graduation()
+    pspict.grid.Dx=200
     pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
     fig.conclude()
     fig.write_the_file()
