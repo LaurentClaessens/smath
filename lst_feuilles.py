28a012ff956382de06b53bdef671e27748c6d240
Laurent Claessens <moky.math@gmail.com>
2013-01-17 09:38:12 +0100

    dm commun

--- lst_feuilles.py
+++ lst_feuilles.py
@@ -5,7 +5,6 @@ import LaTeXparser
 import LaTeXparser.PytexTools
 
 myRequest = LaTeXparser.PytexTools.Request("feuilles")
-myRequest.original_filename="feuilles.tex"
+myRequest.original_filename="exos_interros.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
-
