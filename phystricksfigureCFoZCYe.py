8df47b4681a42327b74c205989bfacd1ed6b8991
Laurent Claessens <moky.math@gmail.com>
2013-01-17 08:51:30 +0100

    17 jan 2013

--- phystricksfigureCFoZCYe.py
+++ phystricksfigureCFoZCYe.py
@@ -2,12 +2,16 @@
 from phystricks import *
 def figureCFoZCYe():
     pspict,fig = SinglePicture("figureCFoZCYe")
-    pspict.dilatation(1)
+    pspict.dilatation_Y(0.5)
 
     x=var('x')
     f=phyFunction(2*x+1).graph(-3,3)
     g=phyFunction(2*x-2).graph(-3,3)
-    h=phyFunction(3*x-1).graph(-3,3)
+    h=phyFunction(3*x-1).graph(-2,3)
+
+    f.parameters.color="blue"
+    g.parameters.color="red"
+    h.parameters.color="brown"
 
     f.put_mark(0.2,0,"\( f\)",automatic_place=pspict)
     g.put_mark(0.2,0,"\( g\)",automatic_place=pspict)
