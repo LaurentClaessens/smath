4fb0d91549096dbe3f31776fef5918ddc01f1797
Laurent Claessens <moky.math@gmail.com>
2013-02-08 13:24:35 +0100

    des exos.

--- lst_seconde.py
+++ lst_seconde.py
@@ -7,4 +7,4 @@ import LaTeXparser.PytexTools
 myRequest = LaTeXparser.PytexTools.Request("seconde")
 myRequest.original_filename="smath.tex"
 
-myRequest.ok_filenames_list=["e_smath","seconde","second_deg","repere","equationDroites","fonctions","stat_Pauline","exercices_stats_descriptives","calcul_mental","rappels_geom","perspective","vecteurs","algo","ineqs","affines","IFetIC"]
+myRequest.ok_filenames_list=["e_smath","seconde","second_deg","repere","equationDroites","fonctions","stat_Pauline","exercices_stats_descriptives","calcul_mental","rappels_geom","perspective","vecteurs","algo","ineqs","affines","IFetIC","proba"]
