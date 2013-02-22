7b52c1ec2ad470cf939e21f092585586051c5e45
Laurent Claessens <moky.math@gmail.com>
2013-02-22 14:41:42 +0100

    Intervalles de confiance de première.

--- phystricksJRVlexw.py
+++ phystricksJRVlexw.py
@@ -6,19 +6,27 @@ from scipy import stats
 def JRVlexw():
     pspict,fig = SinglePicture("JRVlexw")
     pspict.dilatation_X(0.7)
-    pspict.dilatation_Y(50)
+    pspict.dilatation_Y(60)
 
     n=100
     p=0.16  # Ces nombres n et p sont en durs dans l'exemple du médecin dans le chapitre sur l'intervalle de confiance de 1stmg.
+    mx=5
+    Mx=29
     V=stats.binom(n,p)
-    X=list(range(5,30))
+    X=list(range(mx,Mx))
     Y=[V.pmf(k) for k in X]
     Bar=BarDiagram(X,Y)
+    #Bar.numbering=False
+    Bar.numbering_decimals=3
+    #Bar.lines_list[3].parameters.color="red"
     Bar.linewidth=0.3
 
-    pspict.DrawGraphs(Bar,Bar.bounding_box(pspict))
+    ax=SingleAxe(  Point(0,0),Vector(1,0), mx,Mx  )
+
+    pspict.DrawGraphs(Bar,ax)
     #pspict.axes.single_axeX.Dx=5
     #pspict.axes.single_axeY.Dx=0.01
     #pspict.DrawDefaultAxes()
+    fig.no_figure()
     fig.conclude()
     fig.write_the_file()
