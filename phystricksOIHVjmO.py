db37d650ccbd5d24821e6eb98648cf800cac6586
Laurent Claessens <moky.math@gmail.com>
2013-03-06 17:27:58 +0100

    exos inéquations.

---
+++ phystricksOIHVjmO.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def OIHVjmO():
+    pspict,fig = SinglePicture("OIHVjmO")
+    pspict.dilatation_Y(0.2)
+
+    ymin=-10
+    ymax=35
+    x=var('x')
+    f=phyFunction(   (x+1)*(1-x)/(x+3)    ).graph(-10,5)
+    f.cut_y(ymin,ymax,plotpoints=3000)
+
+    v=Segment( Point(-3,ymin),Point(-3,ymax)  )
+    v.parameters.color="red"
+
+    pspict.DrawGraphs(f,v)
+    pspict.axes.single_axeY.Dx=5
+    pspict.DrawDefaultAxes()
+    #pspict.DrawDefaultGrid()
+    fig.conclude()
+    fig.write_the_file()
