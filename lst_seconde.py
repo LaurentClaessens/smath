bebfcc543fda229fabe26ad71076cec24f29a386
Laurent Claessens <moky.math@gmail.com>
2013-03-07 22:05:06 +0100

    divers.

--- lst_seconde.py
+++ lst_seconde.py
@@ -7,4 +7,4 @@ import LaTeXparser.PytexTools
 myRequest = LaTeXparser.PytexTools.Request("seconde")
 myRequest.original_filename="smath.tex"
 
-myRequest.ok_filenames_list=["e_smath","seconde","second_deg","repere","equationDroites","fonctions","stat_Pauline","exercices_stats_descriptives","calcul_mental","rappels_geom","perspective","vecteurs","algo","ineqs","affines","IFetIC","proba"]
+myRequest.ok_filenames_list=["e_smath","seconde","second_deg","repere","equationDroites","fonctions","stat_Pauline","exercices_stats_descriptives","calcul_mental","rappels_geom","perspective","vecteurs","algo","ineqs","affines","IFetIC","proba","fonction_carre","fonction_inverse","fonction_homographique","systeme","trigono","Config_plan","stat_descriptives"]
