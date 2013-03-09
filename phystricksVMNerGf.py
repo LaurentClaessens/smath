6e9ac0c5d2b7f20d8b889260b13b1f3ec8af8517
Laurent Claessens <moky.math@gmail.com>
2013-03-09 06:48:44 +0100

    Prochains DS et DM.

--- phystricksVMNerGf.py
+++ phystricksVMNerGf.py
@@ -5,8 +5,8 @@ def VMNerGf():
     pspict,fig = SinglePicture("VMNerGf")
     pspict.dilatation(1)
 
-    A=Point(1,0)
-    B=Point(5,0)
+    A=Point(0,0)
+    B=Point(4,0)
     v1=Vector(1,1).fix_origin(A)
     v2=Vector(-1,3).fix_origin(B)
 
