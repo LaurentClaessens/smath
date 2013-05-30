b757dae12c18b8964b1370598473a71b01244a97
Laurent Claessens <moky.math@gmail.com>
2013-05-30 18:39:08 +0000

    exo d'interro

--- cree_exo.py
+++ cree_exo.py
@@ -25,12 +25,45 @@ def expression():
     #print(s,A,B,C)
     return s.replace("A",A).replace("B",B).replace("C",C).replace("+-","-")
 
-def exo():
+def exo_facto():
     consigne="""Factoriser l'expression $EXPRESSION$. Pour quelle valeur de \( x\) est-elle nulle ?
     \\vspace{2cm}
     """
     exp=expression()
     return consigne.replace("EXPRESSION",exp)
 
+def exo_trig():
+    paires_angles=[]
+    paires_angles.append([" \( \pi\)", "\( -\pi\) "])
+    paires_angles.append([r"\( \frac{ \pi }{ 4 }\)", r"\( -\frac{ \pi }{ 4 }\) "])
+    paires_angles.append([r"\( \frac{ \pi }{ 3 }\)", r"\( \frac{ 2\pi }{ 3 }\) "])
+    paires_angles.append([r"\( \frac{ \pi }{ 4 }\)", r"\(  \frac{ 3\pi }{ 4 } \) "])
+    paires_angles.append([r"\( \frac{ 10\pi }{ 4 }\)", r"\(  \frac{ 2\pi }{ 4 } \) "])
+    paires_angles.append([r"\( \frac{ 10\pi }{ 4 }\)", r"\(  \frac{ 6\pi }{ 4 } \) "])
+    paires_angles.append([r"\( \frac{ \pi }{ 2 }\)", r"\( \frac{ 5\pi }{ 2 }\) "])
+    paires_angles.append([r"\( \frac{ 2\pi }{ 3 }\)", r"\( -\frac{ 2\pi }{ 3 }\) "])
+    operations=["sin","cos"]
+    paire1=random.choice(paires_angles)
+    paire2=paire1
+    while paire2==paire1:
+        paire2=random.choice(paires_angles)
+    paire3=random.choice(paires_angles)
+    op1=random.choice(operations)
+    op2=random.choice(operations)
+    op1=op2 # Ici je demande qu'on ne compare pas sin et cos.
+    consigne="""Vrai ou faux ? Vous pouvez justifier par un dessin.
+    \\begin{{enumerate}}
+    \item
+    Les nombres {0} et {1} ont même image sur le cercle trigonométrique.
+    \item
+    Les nombres {2} et {3} ont même image sur le cercle trigonométrique.
+    \item
+    {4}({5})={6}({7})
+    \end{{enumerate}}
+    \\vspace{{2cm}}
+    """.format(paire1[0],paire1[1],paire2[0],paire2[1],op1,paire3[0],op2,paire3[1])
+    return consigne
+
 for i in range(1,40):
-    print("{}.".format(i),exo())
+    print("{}.".format(i),exo_trig())
+
