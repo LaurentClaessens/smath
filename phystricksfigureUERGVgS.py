0fcce1cb0e1cb9c382c278b77b41d603787c3a6b
Laurent Claessens <moky.math@gmail.com>
2013-09-18 08:42:53 +0000

    Chapitre sur les fonctions affines.

--- phystricksfigureUERGVgS.py
+++ phystricksfigureUERGVgS.py
@@ -7,25 +7,31 @@ def figureUERGVgS():
     x=var('x')
     f=phyFunction(3*x/4+3).graph(-3,4)
 
+    l=2  # C'est la distance à laquelle on met le second point
+    O=Point(0,0)
+    O.put_mark(0.2,225,"\( O\)",automatic_place=pspict)
     B=Point(0,f(0))
-    B.put_mark(0.2,180,"\( b\)",automatic_place=pspict)
-    A=Point(1,f(1))
-    X=Point(1,f(0))
+    #B.put_mark(0.2,135,"\( p\)",automatic_place=pspict)
+    A=Point(2,f(l))
+    X=Point(l,f(0))
+    I=Point(l,0)
+    I.put_mark(0.2,-90,"\( I\)",automatic_place=(pspict,"N"))
     seg=Segment(B,X)
     seg.parameters.style="dashed"
 
     measure=MeasureLength(Segment(X,A),0.1)
-    measure.put_mark(0.2,0,"\( a\)",automatic_place=pspict)
+    measure.put_mark(0.2,0,"\( m\)",automatic_place=pspict)
 
-    O=Point(0,0)
+    measureB=MeasureLength(Segment(O,B),0.2)
+    measureB.put_mark(0.2,0,"\( p\)",automatic_place=pspict)
+
     #I=Point(1,0)
     #J=Point(0,1)
 
     #I.put_mark(0.2,-90,"\( I\)",automatic_place=pspict)
     #J.put_mark(0.2,180,"\( J\)",automatic_place=pspict)
-    O.put_mark(0.2,90+14,"\( O\)",automatic_place=pspict)
 
-    pspict.DrawGraphs(f,O,B,seg,measure)
+    pspict.DrawGraphs(f,O,B,seg,measure,I,measureB)
 
     pspict.axes.no_graduation()
     pspict.DrawDefaultAxes()
