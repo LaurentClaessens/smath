fe0b182048c5f7819ca658e44433aee629459009
Laurent Claessens <moky.math@gmail.com>
2013-09-29 14:50:20 +0000

    Encore pour le DS

---
+++ phystricksZQfGNsD.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def ZQfGNsD():
+    pspict,fig = SinglePicture("ZQfGNsD")
+    pspict.dilatation(1)
+
+    x=var('x')
+    f=phyFunction(2*x).graph(-1.5,3)
+    g=phyFunction(-x+4).graph(-1.5,5)
+
+    f.put_mark(0.2,0,"\( f\)",automatic_place=(pspict,"W"))
+    g.put_mark(0.2,0,"\( g\)",automatic_place=(pspict,"W"))
+
+    g.parameters.color="brown"
+
+    pspict.DrawGraphs(f,g)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
