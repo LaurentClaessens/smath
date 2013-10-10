9e7613fabd1e51088f5fc26804e8aa9032345c9c
Laurent Claessens <moky.math@gmail.com>
2013-10-10 17:19:25 +0200

    hue

--- phystricksAZKoIsL.py
+++ phystricksAZKoIsL.py
@@ -10,18 +10,18 @@ def AZKoIsL():
     # il faudra changer le texte dans la question.
 
     f=phyFunction((x+2)**2-1).graph(-4,0) 
-    #g=-HermiteInterpolation( [(-3,0,-1),(-2,-1,0),(0,2,0),(1,0,0)] ).graph(-3,1.3)  
-    h=HermiteInterpolation([(-4,-2,0),(-1,1,0),(2,-1,0)]).graph(-4.5,4) 
+    #h=-HermiteInterpolation( [(-3,0,-1),(-2,-1,0),(0,2,0),(1,0,0)] ).graph(-3,1.3)  
+    g=HermiteInterpolation([(-4,-2,0),(-1,1,0),(2,-1,0)]).graph(-4.5,4) 
 
     f.put_mark(0.2,0,"\( f\)",automatic_place=(pspict,"W"))
-    #g.put_mark(0.2,0,"\( g\)",automatic_place=(pspict,"W"))
-    h.put_mark(0.2,0,"\( h\)",automatic_place=(pspict,"W"))
+    #h.put_mark(0.2,0,"\( h\)",automatic_place=(pspict,"W"))
+    g.put_mark(0.2,0,"\( g\)",automatic_place=(pspict,"W"))
 
     f.parameters.color="blue"
-    h.parameters.color="brown"
+    g.parameters.color="brown"
 
     #pspict.DrawGraphs(f,g,h)
-    pspict.DrawGraphs(f,h)
+    pspict.DrawGraphs(f,g)
     pspict.DrawDefaultAxes()
     pspict.DrawDefaultGrid()
     fig.no_figure()
