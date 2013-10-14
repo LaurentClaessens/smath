39573f7001c17793ee724451f4753bc4cb8b5788
Laurent Claessens <moky.math@gmail.com>
2013-10-14 12:01:02 +0200

    Préparation d'un DS

---
+++ phystricksPXsdjSu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def PXsdjSu():
+    pspict,fig = SinglePicture("PXsdjSu")
+    pspict.dilatation(1)
+
+    x=var('x')
+    f=LagrangePolynomial([Point(-1,-0),Point(0,0),Point(3,2),Point(6,-3)]).graph(-1.5,5.3)
+    f.put_mark(0.2,0,"\( f\)",automatic_place=(pspict,"W"))
+
+    pspict.DrawGraphs(f)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
