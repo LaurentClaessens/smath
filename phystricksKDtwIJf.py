33b5c6aa68bd541187ec6d5fbfb2c736d1909241
Laurent Claessens <moky.math@gmail.com>
2013-11-01 07:59:09 +0100

    statistique descriptive.

--- phystricksKDtwIJf.py
+++ phystricksKDtwIJf.py
@@ -9,14 +9,24 @@ def KDtwIJf():
 
     minimum=0
     Maximum=100
-    Q1=26
-    mediane=50
-    Q3=76
+    Q1=30
+    mediane=55
+    Q3=80
     moustache=Moustache(minimum,Q1,mediane,Q3,Maximum,h=0.5,delta_y=0.75)
 
+    sm1=Segment(Point(minimum,0),Point(Q1,0))
+    m1=MeasureLength(sm1,0.2)
+    m1.put_mark(0.3,m1.advised_mark_angle,"Au moins \( \\frac{ 1 }{ 4 }\)")
 
+    sm2=Segment(Point(minimum,1.5),Point(Q3,1.5))
+    m2=MeasureLength(sm2,0.2)
+    m2.put_mark(-0.3,m2.advised_mark_angle,"Au moins \( \\frac{ 3 }{ 4 }\)")
 
-    pspict.DrawGraphs(moustache)
+    sm3=Segment(Point(Q3,0),Point(Maximum,0))
+    m3=MeasureLength(sm3,0.2)
+    m3.put_mark(0.3,m3.advised_mark_angle,"\( \\frac{ 1 }{ 4 }\) ou moins")
+
+    pspict.DrawGraphs(moustache,m1,m2,m3)
 
     #pspict.DrawDefaultAxes()
     #pspict.axes.draw_single_axeY=False
--- phystricksYRQOoPE.py
+++ phystricksYRQOoPE.py
@@ -13,6 +13,10 @@ def YRQOoPE():
         s.parameters.add_option("linewidth","1.5mm")
         pspict.DrawGraphs(s)
 
+    M=Point(9,0)
+    M.put_mark(0.5,-90,"Nombre de spam",automatic_place=(pspict,"N"))
+    pspict.single_axeY.put_mark(0.2,0,"Effectifs",automatic_place=(pspict,"W"))
+    pspict.DrawGraphs(M)
     pspict.DrawDefaultAxes()
     fig.no_figure()
     fig.conclude()
