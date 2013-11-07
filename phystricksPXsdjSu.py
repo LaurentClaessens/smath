72112eb4ee96cb016fa10c4deba5622d9432a38b
Laurent Claessens <moky.math@gmail.com>
2013-11-07 11:34:36 +0100

    typos dans des exercices.

--- phystricksPXsdjSu.py
+++ phystricksPXsdjSu.py
@@ -2,7 +2,8 @@
 from phystricks import *
 def PXsdjSu():
     pspict,fig = SinglePicture("PXsdjSu")
-    pspict.dilatation(1)
+    pspict.dilatation_X(1)
+    pspict.dilatation_Y(1)
 
     x=var('x')
     f=LagrangePolynomial([Point(-1,-0),Point(0,0),Point(3,2),Point(6,-3)]).graph(-1.5,5.3)
