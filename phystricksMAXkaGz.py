db032a29d0b9fa98e501319578a6c29cc3dd3d40
Laurent Claessens <moky.math@gmail.com>
2013-11-13 20:53:31 +0100

    modified:   phystricksMAXkaGz.py

--- phystricksMAXkaGz.py
+++ phystricksMAXkaGz.py
@@ -30,6 +30,7 @@ def MAXkaGz():
         P=Point(i+1,cum)
         Q=Point(i+1,0)
         Q.put_mark(0.2,-90,c,automatic_place=(pspict,"N"))
+        Q.parameters.symbol="none"
         pspict.DrawGraphs(P,Q)
 
 
