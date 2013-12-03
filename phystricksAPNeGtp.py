66e4e42b98a00a222c4c86cb7ef2f3fb9390f1de
Laurent Claessens <moky.math@gmail.com>
2013-12-03 20:01:41 +0000

    quelque doublons.

--- phystricksAPNeGtp.py
+++ phystricksAPNeGtp.py
@@ -5,8 +5,8 @@ from phystricks import *
 
 def APNeGtp():
     pspict,fig = SinglePicture("APNeGtp")
-    pspict.dilatation_X(1)
-    pspict.dilatation_Y(1)
+    pspict.dilatation_X(0.8)
+    pspict.dilatation_Y(0.8)
 
     f=phyFunction(x*(x-1)*(x+3)/2).graph(-3.5,1.5)
 
