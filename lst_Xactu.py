a025e3d5240a6fbd4ac56d0f8c9d91940071392c
Laurent Claessens <moky.math@gmail.com>
2013-12-26 12:37:10 +0000

    Chapitre sur la fluctuation et préparation pour les vecteurs.

--- lst_Xactu.py
+++ lst_Xactu.py
@@ -7,5 +7,4 @@ import LaTeXparser.PytexTools
 myRequest = LaTeXparser.PytexTools.Request("seconde")
 myRequest.original_filename="smath.tex"
 
-myRequest.ok_filenames_list=["e_smath"]
-myRequest.ok_filenames_list.append("0080_IFetIC")
+myRequest.ok_filenames_list=["e_smath","0100_smath","0110_smath"]
