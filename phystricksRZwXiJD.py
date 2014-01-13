b1641e1a8a25a444bb01316c30dc4aa2a23001b9
Laurent Claessens <moky.math@gmail.com>
2014-01-13 16:37:01 +0000

    Les prochains 100 exercices

---
+++ phystricksRZwXiJD.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def RZwXiJD():
+    pspict,fig = SinglePicture("RZwXiJD")
+    pspict.dilatation_X(1)
+    pspict.dilatation_Y(1)
+
+    l=3
+    perspective=ObliqueProjection(30,0.5)
+    cube=perspective.cuboid((0,0),l,l,l)
+    cube.put_vertex_mark(pspict)
+
+
+    I=Segment(cube.E,cube.F).center()
+    J=Segment(cube.B,cube.F).center()
+
+    I.put_mark(0.2,90,"\( I\)",automatic_place=(pspict,"S"))
+    J.put_mark(0.2,-45,"\( J\)",automatic_place=(pspict,"corner"))
+
+    pspict.DrawGraphs(cube,I,J)
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
