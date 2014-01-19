3eb2c194fccb3d8c854d97d0b01b9a6857f0880b
Laurent Claessens <moky.math@gmail.com>
2014-01-19 10:40:11 +0000

    correction DS.

--- phystricksGSfDCyx.py
+++ phystricksGSfDCyx.py
@@ -5,15 +5,18 @@ def GSfDCyx():
     pspict.dilatation_X(0.8)
     pspict.dilatation_Y(1)
 
-    moustache_DS_D1=Moustache(1.5,6.5,9.75,14.025,17.0,h=0.5,delta_y=2.75)
-    moustache_DS_D2=Moustache(1.0,8.5,12.0,14.5,19.0,h=0.5,delta_y=1.75)
-    moustache_DS_D3=Moustache(0.0,8.1,11.0,14.0,17.5,h=0.5,delta_y=0.75)
+    moustaches=[]
 
-    moustache_DS_D1.put_mark(0.2,0,"DS 1",automatic_place=(pspict,"W"))
-    moustache_DS_D2.put_mark(0.2,0,"DS 2",automatic_place=(pspict,"W"))
-    moustache_DS_D3.put_mark(0.2,0,"DS 3",automatic_place=(pspict,"W"))
+    moustaches.append(Moustache(5.5,11.0,12.5,14.32,18.5,h=0.5,delta_y=0.75))      #DS4
+    moustaches.append(Moustache(0.0,8.1,11.0,14.0,17.5,h=0.5,delta_y=0.75))        #DS3
+    moustaches.append(Moustache(1.0,8.5,12.0,14.5,19.0,h=0.5,delta_y=1.75))        #DS2
+    moustaches.append(Moustache(1.5,6.5,9.75,14.025,17.0,h=0.5,delta_y=2.75))      #DS1
 
-    pspict.DrawGraphs(moustache_DS_D1,moustache_DS_D2,moustache_DS_D3)
+    for i,m in enumerate(moustaches) :
+        m.delta_y=0.75+i
+        m.put_mark(0.2,0,"DS {}".format( len(moustaches)-i ),automatic_place=(pspict,"W"))
+
+    pspict.DrawGraphs(moustaches)
     pspict.grid.draw_horizontal_grid=False
     pspict.axes.draw_single_axeY=False
     pspict.axes.single_axeY.no_graduation()
--- phystricksPEoyeQt.py
+++ phystricksPEoyeQt.py
@@ -5,15 +5,18 @@ def PEoyeQt():
     pspict.dilatation_X(0.8)
     pspict.dilatation_Y(1)
 
-    moustache_DS_A1=Moustache(2.5,6.5,8.5,11.5,17.0,h=0.5,delta_y=2.75)
-    moustache_DS_A2=Moustache(1.0,5.5,7,11.4,19.0,h=0.5,delta_y=1.75)
-    moustache_DS_A3=Moustache(3.0,5.6,7.5,10.9,15.5,h=0.5,delta_y=0.75)
+    moustaches=[]
 
-    moustache_DS_A1.put_mark(0.2,0,"DS 1",automatic_place=(pspict,"W"))
-    moustache_DS_A2.put_mark(0.2,0,"DS 2",automatic_place=(pspict,"W"))
-    moustache_DS_A3.put_mark(0.2,0,"DS 3",automatic_place=(pspict,"W"))
+    moustaches.append(Moustache(2.0,7.5,10.0,12.0,19.5,h=0.5,delta_y=0.75))     #DS4
+    moustaches.append(Moustache(3.0,5.6,7.5,10.9,15.5,h=0.5,delta_y=0.75))      #DS3
+    moustaches.append(Moustache(1.0,5.5,7,11.4,19.0,h=0.5,delta_y=1.75))        #DS2
+    moustaches.append(Moustache(2.5,6.5,8.5,11.5,17.0,h=0.5,delta_y=2.75))      #DS1
 
-    pspict.DrawGraphs(moustache_DS_A1,moustache_DS_A2,moustache_DS_A3)
+    for i,m in enumerate(moustaches) :
+        m.delta_y=0.75+i
+        m.put_mark(0.2,0,"DS {}".format( len(moustaches)-i ),automatic_place=(pspict,"W"))
+
+    pspict.DrawGraphs(moustaches)
     pspict.grid.draw_horizontal_grid=False
     pspict.axes.draw_single_axeY=False
     pspict.axes.single_axeY.no_graduation()
