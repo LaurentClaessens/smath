703bef698c022ccfb07af9571e7caa8500d3bdb6
Laurent Claessens <moky.math@gmail.com>
2014-01-19 15:42:24 +0000

    Correction du DS4

--- figures_smath.py
+++ figures_smath.py
@@ -4,6 +4,7 @@
 from phystricks import *
 import sys
 
+from phystricksWTVlzUE import WTVlzUE
 from phystricksRHfkPKj import RHfkPKj
 from phystricksRZwXiJD import RZwXiJD
 from phystricksGTyQVvw import GTyQVvw
@@ -267,7 +268,7 @@ figures_list=[ExoCUd,SolsEqDiffSin,Grapheunsurunmoinsx,TracerUn,ExerciceGraphes,
     XSMDwcv,ITZAPPh,KQluTdN,IXtyOnk,FLnDVHh,CZAVGrm,OIHVjmO,OSQOqJN,BZqEWco,AVFexUi,LSaSLoS,VDqUhPV,KMJQWwa,UEqUrvi,DZmbmzZ,ITlywMb,EGDFJAT,AHAbqhj,WRXbDCo,LNfaWPF,GRRhrJe,
     AZKoIsL,FPIkJJx,OWGRRSC,ZQfGNsD,IRHrmQQ,KGOAveW,PXsdjSu,XPqLRCF, MEzTDZC,ONMRllE,IDqyzXM,ETfnbsh,ENQhxmG,YRQOoPE,KDtwIJf,MAXkaGz,LTenBUj,ALzPUfm,LQGzkvL,APYEkIv,UKBImJl,
     MUriGyU,WGhpCVm,HMNXfhy,PYYEasw,DNYAefI,FWyrYhJ,YGlrtNX,EDnzyzS,BZRzIsR,VXFxyni,YBxSjFS,ITzxISE,RLGQTQR,WYeESAN,EIxhcRb,OKTXHoc,JXWXdJI,PUGmLBC,LZDNqtV, APNeGtp,QLbEnnx,
-    JLUTBlD,FFyFBoe,RRlBSZg,PEoyeQt,GSfDCyx,EJbcoxO,ILauamX,JWNtVIR,EDEYRhQ,WPrirwB,OZfyDkr,KYbSnVB,AILoxBg,AUHmNET,XDdastq,GTyQVvw,RZwXiJD,RHfkPKj
+    JLUTBlD,FFyFBoe,RRlBSZg,PEoyeQt,GSfDCyx,EJbcoxO,ILauamX,JWNtVIR,EDEYRhQ,WPrirwB,OZfyDkr,KYbSnVB,AILoxBg,AUHmNET,XDdastq,GTyQVvw,RZwXiJD,RHfkPKj,WTVlzUE
     ]
 
 #figures_list=[PositionPlansTvKvah,PositionPlansqSltxa,figureBCtCTZo]
---
+++ phystricksWTVlzUE.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def WTVlzUE():
+    pspict,fig = SinglePicture("WTVlzUE")
+    pspict.dilatation_X(0.5)
+    pspict.dilatation_Y(0.5)
+
+    l=3
+    perspective=ObliqueProjection(30,0.5)
+    cube=perspective.cuboid((0,0),13,5,6)
+    cube.put_vertex_mark(pspict)
+
+    dilatation=1.6
+    diag=Segment(cube.D,cube.F)
+    pdiag=Segment(cube.D,cube.G)
+    diag.parameters.color="red"
+    pdiag.parameters.color="red"
+
+    pspict.DrawGraphs(cube,diag,pdiag)
+
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
