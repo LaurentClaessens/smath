717b40783f18726363987fe4f62d006ab58784bd
Laurent Claessens <moky.math@gmail.com>
2014-02-04 14:27:47 +0000

    Correction de DS

--- figures_smath.py
+++ figures_smath.py
@@ -4,6 +4,7 @@
 from phystricks import *
 import sys
 
+from phystricksZMGMLvNBa import ZMGMLvNBa
 from phystricksSWFywZG import SWFywZG
 from phystricksDDRbyQk import DDRbyQk
 from phystricksQGRiHbb import QGRiHbb
@@ -277,7 +278,7 @@ figures_list=[ExoCUd,SolsEqDiffSin,Grapheunsurunmoinsx,TracerUn,ExerciceGraphes,
     AZKoIsL,FPIkJJx,OWGRRSC,ZQfGNsD,IRHrmQQ,KGOAveW,PXsdjSu,XPqLRCF, MEzTDZC,ONMRllE,IDqyzXM,ETfnbsh,ENQhxmG,YRQOoPE,KDtwIJf,MAXkaGz,LTenBUj,ALzPUfm,LQGzkvL,APYEkIv,UKBImJl,
     MUriGyU,WGhpCVm,HMNXfhy,PYYEasw,DNYAefI,FWyrYhJ,YGlrtNX,EDnzyzS,BZRzIsR,VXFxyni,YBxSjFS,ITzxISE,RLGQTQR,WYeESAN,EIxhcRb,OKTXHoc,JXWXdJI,PUGmLBC,LZDNqtV, APNeGtp,QLbEnnx,
     JLUTBlD,FFyFBoe,RRlBSZg,PEoyeQt,GSfDCyx,EJbcoxO,ILauamX,JWNtVIR,EDEYRhQ,WPrirwB,OZfyDkr,KYbSnVB,AILoxBg,AUHmNET,XDdastq,GTyQVvw,RZwXiJD,RHfkPKj,WTVlzUE,XMjsBcU,UZlaYZ,
-    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG
+    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG,ZMGMLvNBa
     ]
 
 #figures_list=[PositionPlansTvKvah,PositionPlansqSltxa,figureBCtCTZo]
---
+++ phystricksZMGMLvNBa.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def ZMGMLvNBa():
+    pspict,fig = SinglePicture("ZMGMLvNBa")
+    pspict.dilatation_X(1)
+    pspict.dilatation_Y(1)
+
+    K=Point(-5,0)
+    A=Point(0,3)
+    B=Point(2,7)
+    C=Point(6,5)
+    D=Point(7,4)
+
+    s1=Segment(K,A)
+    s2=Segment(A,B)
+    s3=Segment(B,C)
+    s4=Segment(C,D)
+
+    K.put_mark(0.2,90,"\( K\)",automatic_place=(pspict,"S"))
+    A.put_mark(0.2,-45,"\( A\)",automatic_place=(pspict,"corner"))
+    B.put_mark(0.2,90,"\( B\)",automatic_place=(pspict,"N"))
+    C.put_mark(0.2,45,"\( C\)",automatic_place=(pspict,"corner"))
+    D.put_mark(0.2,0,"\( D\)",automatic_place=(pspict,"W"))
+
+    pspict.DrawGraphs(K,A,B,C,D,s1,s2,s3,s4)
+    pspict.DrawDefaultAxes()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
+
