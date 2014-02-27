75262263ab04e5e2c7337fbf79f1d8459bf608b6
Laurent Claessens <moky.math@gmail.com>
2014-02-27 13:51:25 +0000

    Fonctions du second degré.

--- figures_smath.py
+++ figures_smath.py
@@ -4,6 +4,7 @@
 from phystricks import *
 import sys
 
+from phystricksOLuvnaY import OLuvnaY
 from phystricksIBmsroy import IBmsroy
 from phystricksGUEjmmR import GUEjmmR
 from phystricksCFjmTFb import CFjmTFb
@@ -283,7 +284,7 @@ figures_list=[ExoCUd,SolsEqDiffSin,Grapheunsurunmoinsx,TracerUn,ExerciceGraphes,
     AZKoIsL,FPIkJJx,OWGRRSC,ZQfGNsD,IRHrmQQ,KGOAveW,PXsdjSu,XPqLRCF, MEzTDZC,ONMRllE,IDqyzXM,ETfnbsh,ENQhxmG,YRQOoPE,KDtwIJf,MAXkaGz,LTenBUj,ALzPUfm,LQGzkvL,APYEkIv,UKBImJl,
     MUriGyU,WGhpCVm,HMNXfhy,PYYEasw,DNYAefI,FWyrYhJ,YGlrtNX,EDnzyzS,BZRzIsR,VXFxyni,YBxSjFS,ITzxISE,RLGQTQR,WYeESAN,EIxhcRb,OKTXHoc,JXWXdJI,PUGmLBC,LZDNqtV, APNeGtp,QLbEnnx,
     JLUTBlD,FFyFBoe,RRlBSZg,PEoyeQt,GSfDCyx,EJbcoxO,ILauamX,JWNtVIR,EDEYRhQ,WPrirwB,OZfyDkr,KYbSnVB,AILoxBg,AUHmNET,XDdastq,GTyQVvw,RZwXiJD,RHfkPKj,WTVlzUE,XMjsBcU,UZlaYZ,
-    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG,ZMGMLvNBa,NIGYQHN,OCFmVDE,CFjmTFb,GUEjmmR,IBmsroy
+    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG,ZMGMLvNBa,NIGYQHN,OCFmVDE,CFjmTFb,GUEjmmR,IBmsroy,OLuvnaY
     ]
 
 #figures_list=[PositionPlansTvKvah,PositionPlansqSltxa,figureBCtCTZo]
---
+++ phystricksOLuvnaY.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def OLuvnaY():
+    pspict,fig = SinglePicture("OLuvnaY")
+    pspict.dilatation_X(0.7)
+    pspict.dilatation_Y(0.7)
+
+    c=5
+    A=Point(0,0)
+    B=A+(c,0)
+    C=B+(0,c)
+    D=C+(-c,0)
+
+    gd_carre=Polygon(A,B,C,D)
+    l=2
+    N=D+(l,0)
+    M=B+(0,l)
+    P=Point(N.x,M.y)
+
+    pt_carre=Polygon(N,P,M,C)
+    gris=Polygon(N,P,M,A)
+    gris.parameters.filled()
+    gris.parameters.fill.color="lightgray"
+
+    A.put_mark(0.2,180+45,"\( A\)",automatic_place=(pspict,"corner"))
+    B.put_mark(0.2,-45,"\( B\)",automatic_place=(pspict,"corner"))
+    C.put_mark(0.2,45,"\( C\)",automatic_place=(pspict,"corner"))
+    D.put_mark(0.2,90+45,"\( D\)",automatic_place=(pspict,"corner"))
+    N.put_mark(0.2,90,"\( N\)",automatic_place=(pspict,"S"))
+    P.put_mark(0.2,45,"\( P\)",automatic_place=(pspict,"corner"))
+    M.put_mark(0.2,0,"\( M\)",automatic_place=(pspict,"W"))
+
+
+    pspict.DrawGraphs(gd_carre,pt_carre,gris,A,B,C,D,N,P,M)
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
