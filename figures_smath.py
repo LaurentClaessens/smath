451f88288238f62ccb5bb0b7a320afc6c792bfeb
Laurent Claessens <moky.math@gmail.com>
2014-02-28 14:43:02 +0000

    Exos vecteurs.

--- figures_smath.py
+++ figures_smath.py
@@ -4,6 +4,8 @@
 from phystricks import *
 import sys
 
+
+from phystricksQGaeERu import QGaeERu
 from phystricksQFpJtQc import QFpJtQc
 from phystricksOLuvnaY import OLuvnaY
 from phystricksIBmsroy import IBmsroy
@@ -285,7 +287,7 @@ figures_list=[ExoCUd,SolsEqDiffSin,Grapheunsurunmoinsx,TracerUn,ExerciceGraphes,
     AZKoIsL,FPIkJJx,OWGRRSC,ZQfGNsD,IRHrmQQ,KGOAveW,PXsdjSu,XPqLRCF, MEzTDZC,ONMRllE,IDqyzXM,ETfnbsh,ENQhxmG,YRQOoPE,KDtwIJf,MAXkaGz,LTenBUj,ALzPUfm,LQGzkvL,APYEkIv,UKBImJl,
     MUriGyU,WGhpCVm,HMNXfhy,PYYEasw,DNYAefI,FWyrYhJ,YGlrtNX,EDnzyzS,BZRzIsR,VXFxyni,YBxSjFS,ITzxISE,RLGQTQR,WYeESAN,EIxhcRb,OKTXHoc,JXWXdJI,PUGmLBC,LZDNqtV, APNeGtp,QLbEnnx,
     JLUTBlD,FFyFBoe,RRlBSZg,PEoyeQt,GSfDCyx,EJbcoxO,ILauamX,JWNtVIR,EDEYRhQ,WPrirwB,OZfyDkr,KYbSnVB,AILoxBg,AUHmNET,XDdastq,GTyQVvw,RZwXiJD,RHfkPKj,WTVlzUE,XMjsBcU,UZlaYZ,
-    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG,ZMGMLvNBa,NIGYQHN,OCFmVDE,CFjmTFb,GUEjmmR,IBmsroy,OLuvnaY,QFpJtQc
+    OKeZlpK,RVZNtGK,CFFyezr,QGRiHbb,DDRbyQk,SWFywZG,ZMGMLvNBa,NIGYQHN,OCFmVDE,CFjmTFb,GUEjmmR,IBmsroy,OLuvnaY,QFpJtQc,QGaeERu
     ]
 
 #figures_list=[PositionPlansTvKvah,PositionPlansqSltxa,figureBCtCTZo]
---
+++ phystricksQGaeERu.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def QGaeERu():
+    pspict,fig = SinglePicture("QGaeERu")
+    pspict.dilatation_X(0.3)
+    pspict.dilatation_Y(0.045)
+
+    pspict.My_acceptable_BB=200
+
+    x=var('x')
+    f=phyFunction(-x**2+20*x+21).graph(0,21)
+
+    pspict.DrawGraphs(f)
+
+    pspict.axes.single_axeX.Dx=2
+    pspict.axes.single_axeY.Dx=10
+    pspict.DrawDefaultAxes()
+    pspict.grid.Dx=pspict.single_axeX.Dx
+    pspict.grid.Dy=pspict.single_axeY.Dx
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
