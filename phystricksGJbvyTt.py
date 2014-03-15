7cc5048081da35d4eeabbe84772b7bd0cf895df0
Laurent Claessens <moky.math@gmail.com>
2014-03-15 06:46:58 +0000

    Correction d'exercices de DS

--- phystricksGJbvyTt.py
+++ phystricksGJbvyTt.py
@@ -9,16 +9,18 @@ def GJbvyTt():
     A=Point(3,1)
     B=Point(-1,1)
     C=Point(-3,2)
+    F=B+(-1,0)
 
     O.put_mark(0.2,-45,"\( O\)",automatic_place=(pspict,"corner"))
     A.put_mark(0.2,45,"\( A\)",automatic_place=(pspict,"corner"))
     B.put_mark(0.2,180+45,"\( B\)",automatic_place=(pspict,"corner"))
     C.put_mark(0.2,90+45,"\( C\)",automatic_place=(pspict,"corner"))
+    F.put_mark(0.2,180+45,"\( F\)",automatic_place=(pspict,"corner"))
 
     pspict.force_math_bounding_box( C+(-1,1) )
     pspict.force_math_bounding_box( A+(1,0) )
 
-    pspict.DrawGraphs(O,A,C,B)
+    pspict.DrawGraphs(O,A,C,B,F)
     pspict.DrawDefaultGrid()
     fig.no_figure()
     fig.conclude()
