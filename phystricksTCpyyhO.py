edd7f8acac849bd1b9c6f485e698759ad6938904
Laurent Claessens <moky.math@gmail.com>
2014-03-24 15:26:39 +0000

    Fichier manquant

---
+++ phystricksTCpyyhO.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def TCpyyhO():
+    pspict,fig = SinglePicture("TCpyyhO")
+    pspict.dilatation_X(0.7)
+    pspict.dilatation_Y(0.7)
+
+    x=var('x')
+    xmin=-1
+    xmax=8
+    ymin=-0.5
+    ymax=12
+    g=phyFunction(2*x+1).fit_inside(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax)
+    h=phyFunction(3*x-4).fit_inside(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax)
+
+    pspict.DrawGraphs(g,h)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
