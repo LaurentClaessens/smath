255603aa5a4a8a5cc2d288bfd06cb56d3a15aafa
Laurent Claessens <moky.math@gmail.com>
2014-04-03 04:43:38 +0000

    On continue avec le DS 8

---
+++ phystricksLXWooEsxsx.py
@@ -0,0 +1,38 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def LXWooEsxsx():
+    pspict,fig = SinglePicture("LXWooEsxsx")
+    pspict.dilatation_X(1)
+    pspict.dilatation_Y(1)
+
+    fig.specific_needs="""\makeatletter\@ifundefined{vect}{\\newcommand{\\vect}[1]{\overrightarrow{#1}}}{}\makeatother"""
+
+    x=var('x')
+    A1=Point(0,0)
+    B1=Point(1,2)
+    B2=Point(-0.5,1.5)
+
+    m1=Vector(3,-1)
+    m2=Vector(2,0)
+
+    u1=m1.fix_origin(A1)
+    v1=0.5*m1.fix_origin(B1)
+
+    u2=m2.fix_origin(A1)
+    v2=0.5*m2.fix_origin(B2)
+
+    u3=AffineVector(  Point(5,3),Point(2,2) )
+    v3=AffineVector( Point(1,1),Point(0,-2) )
+
+    u1.put_mark(0.2,45,"\( \\vect{ u_1 }\)",automatic_place=(pspict,"corner"))
+    v1.put_mark(0.2,-45,"\( \\vect{ v_1 }\)",automatic_place=(pspict,"corner"))
+    u2.put_mark(0.2,45,"\( \\vect{ u_2 }\)",automatic_place=(pspict,"corner"))
+    v2.put_mark(0.2,90,"\( \\vect{ v_2 }\)",automatic_place=(pspict,"S"))
+    u3.put_mark(0.2,45,"\( \\vect{ u_3 }\)",automatic_place=(pspict,"corner"))
+    v3.put_mark(0.2,-45,"\( \\vect{ v_3 }\)",automatic_place=(pspict,"corner"))
+
+    pspict.DrawGraphs(u1,v1,u2,v2,u3,v3)
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
