597bffffa03efbd5066b2ce57ce0daba40f894b9
Laurent Claessens <moky.math@gmail.com>
2014-05-11 16:05:28 +0200

    Exercices récapitulatifs.

--- phystricksYGlrtNX.py
+++ phystricksYGlrtNX.py
@@ -2,8 +2,7 @@
 from phystricks import *
 def YGlrtNX():
     pspict,fig = SinglePicture("YGlrtNX")
-    pspict.dilatation_X(1)
-    pspict.dilatation_Y(1)
+    pspict.dilatation_X(0.9)
 
     f=LagrangePolynomial( [Point(-4,-2),Point(-3,0),Point(-1.5,2),Point(0,0),Point(1,1),Point(2,3),Point(4,0)] ).graph(-4,4)
 
