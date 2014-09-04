9bd045ab25f2201842bd9b120f9bcb2201e0dd8f
Laurent Claessens <moky.math@gmail.com>
2014-09-04 19:56:28 +0200

    Exercices 5ième

---
+++ lst_actu5.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+
+myRequest = LaTeXparser.PytexTools.Request("seconde")
+myRequest.original_filename="smath.tex"
+
+myRequest.ok_filenames_list=["e_smath"]
+myRequest.ok_filenames_list.append("4_priorite")
+myRequest.ok_filenames_list.append("2_mental5")
+myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("<++>")
