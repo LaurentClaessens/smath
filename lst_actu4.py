b953841336761b00539276d4812f27b8f7121c5d
Laurent Claessens <moky.math@gmail.com>
2014-09-06 08:29:45 +0200

    Exercices pour la quatrième.

--- lst_actu4.py
+++ lst_actu4.py
@@ -9,7 +9,7 @@ myRequest.original_filename="smath.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
 myRequest.ok_filenames_list.append("3_relatifs4")
-myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("6_exercices_feuilles_quatrieme")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
