eeeafedecf36435389c6aad2a6b47ef26c688053
Laurent Claessens <moky.math@gmail.com>
2014-09-09 07:24:20 +0200

    surface -> aire là où il faut.

--- phystricksHHOooQUedri.py
+++ phystricksHHOooQUedri.py
@@ -14,9 +14,9 @@ def HHOooQUedri():
     AB=Segment(A,B)
     AB.put_mark(0.2,AB.advised_mark_angle+180,"\( 4\)",automatic_place=(pspict,"corner"))
     AC=Segment(A,C)
-    AC.put_mark(0.2,AC.advised_mark_angle,"\( 7\)",automatic_place=(pspict,"corner"))
+    AC.put_mark(0.2,AC.advised_mark_angle,"\( 13\)",automatic_place=(pspict,"corner"))
     BC=Segment(B,C)
-    BC.put_mark(0.2,BC.advised_mark_angle+180,"\( 13\)",automatic_place=(pspict,"corner"))
+    BC.put_mark(0.2,BC.advised_mark_angle+180,"\( 7\)",automatic_place=(pspict,"corner"))
 
     pspict.DrawGraphs(triangle,AB,AC,BC)
     fig.no_figure()
