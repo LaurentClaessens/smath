da570004c92c159603ded504fe59946b704fc2f0
Laurent Claessens <moky.math@gmail.com>
2014-09-30 10:19:50 +0200

    Une activité mentale.

--- lst_mental.py
+++ lst_mental.py
@@ -5,14 +5,11 @@ import LaTeXparser
 import LaTeXparser.PytexTools
 
 myRequest = LaTeXparser.PytexTools.Request("seconde")
-myRequest.original_filename="smath.tex"
+myRequest.original_filename="mental.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
 myRequest.ok_filenames_list.append("2_mental5")
-myRequest.ok_filenames_list.append("13_fract")
-myRequest.ok_filenames_list.append("15_evaluation")
-myRequest.ok_filenames_list.append("14_methodologie")
-myRequest.ok_filenames_list.append("7_exercices_feuilles_cinquieme")
+myRequest.ok_filenames_list.append("9_mental4")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
