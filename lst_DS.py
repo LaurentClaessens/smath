8c1b722ed8662c2a70472fca7c2c75c6eb0368d4
Laurent Claessens <moky.math@gmail.com>
2014-10-16 00:02:06 +0200

    Références multiples corrigées.

--- lst_DS.py
+++ lst_DS.py
@@ -11,11 +11,12 @@ groupe_4A=enseignement.groupe_4A
 groupe_5A=enseignement.groupe_5A
 groupe_5B=enseignement.groupe_5B
 
+DS1_5B=commun.TheDS("DS1_5B",groupe_5B,1)
 DS2_5A=commun.TheDS("DS2_5A",groupe_5A,1)
 DS2_5B=commun.TheDS("DS2_5B",groupe_5B,1)
 DS2_4A=commun.TheDS("DS2_4A",groupe_4A,1)
 
-jeveux=DS2_5A
+jeveux=DS2_4A
 jeveux.write_the_file()
 
 myRequest = LaTeXparser.PytexTools.Request("seconde")
