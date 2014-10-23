06cd29b9c57221e649d2633efd63b453331dca6b
Laurent Claessens <moky.math@gmail.com>
2014-10-23 13:38:48 +0200

    stats sur le DS2_5AB

--- phystricksTYIooYKqeNv.py
+++ phystricksTYIooYKqeNv.py
@@ -3,27 +3,15 @@ from phystricks import *
 
 import phystricksCommons
 
-def PJQooTWPTXV():
-    pspict,fig = SinglePicture("PJQooTWPTXV")
-
-    moustaches=[]
-
-    # Le delta_y n'a pas d'importance parce qu'il est recalculé plus bas.
-    moustaches.append(Moustache(6.00,8.99,11.50,14.37,20.00,h=0.5,delta_y=0.75))    #DS1
-
-    phystricksCommons.DS_statistics(moustaches,pspict)
-
-    fig.no_figure()
-    fig.conclude()
-    fig.write_the_file()
-
 def CXWooOMPOQT():
+    # 5A
     pspict,fig = SinglePicture("CXWooOMPOQT")
 
     moustaches=[]
 
     # Le delta_y n'a pas d'importance parce qu'il est recalculé plus bas.
     moustaches.append(Moustache(7.90,10.20,14.45,16.70,20.00,h=0.5,delta_y=0.75))   # DS1
+    moustaches.append(Moustache(5.10,8.75,10.87,16.29,20.00,h=0.5,delta_y=0.75))    # DS2
 
     phystricksCommons.DS_statistics(moustaches,pspict)
 
@@ -31,15 +19,32 @@ def CXWooOMPOQT():
     fig.conclude()
     fig.write_the_file()
 
+def PJQooTWPTXV():
+    # 5B
+    pspict,fig = SinglePicture("PJQooTWPTXV")
+
+    moustaches=[]
+
+    # Le delta_y n'a pas d'importance parce qu'il est recalculé plus bas.
+    moustaches.append(Moustache(6.00,8.99,11.50,14.37,20.00,h=0.5,delta_y=0.75))    # DS1
+    moustaches.append(Moustache(4.93,7.71,11.85,14.03,16.48,h=0.5,delta_y=0.75))    # DS2
+
+    phystricksCommons.DS_statistics(moustaches,pspict)
+
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
+
+
 def JNGEooISdbXx():
-    # Les graphes des deux cinquièmes mélangées.
+    # 5AB mélangés
     pspict,fig = SinglePicture("JNGEooISdbXx")
 
     moustaches=[]
 
     # Le delta_y n'a pas d'importance parce qu'il est recalculé plus bas.
     moustaches.append(Moustache(6.00,10.10,12.10,16.30,20.00,h=0.5,delta_y=0.75))   # DS1
-    moustaches.append(Moustache(4.35,7.45,11.59,15.49,20.00,h=0.5,delta_y=0.75))
+    moustaches.append(Moustache(4.93,8.08,11.45,15.20,20.00,h=0.5,delta_y=0.75))    # DS2
 
     phystricksCommons.DS_statistics(moustaches,pspict)
 
@@ -48,7 +53,6 @@ def JNGEooISdbXx():
     fig.write_the_file()
 
 def TYIooYKqeNv():
-    #PJQooTWPTXV()
-    #CXWooOMPOQT()
+    PJQooTWPTXV()
+    CXWooOMPOQT()
     JNGEooISdbXx()
-
