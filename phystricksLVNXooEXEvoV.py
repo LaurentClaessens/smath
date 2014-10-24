4b765c2022c490e3a87b8a2a5dfb5cd57cdba3f8
Laurent Claessens <moky.math@gmail.com>
2014-10-24 17:15:59 +0200

    Des figures pour des hauteurs et médianes.

--- phystricksLVNXooEXEvoV.py
+++ phystricksLVNXooEXEvoV.py
@@ -1,19 +1,34 @@
 # -*- coding: utf8 -*-
 from phystricks import *
 def LVNXooEXEvoV():
-    pspicts,fig = IndependentPictures("LVNXooEXEvoV",5)
-    pspicts[0].mother.caption="<+caption1+>"
-    pspicts[1].mother.caption="<+caption2+>"
-    pspicts[2].mother.caption="<+caption3+>"
+    pspicts,figs = IndependentPictures("LVNXooEXEvoV",5)
 
     for psp in pspicts:
         psp.dilatation_X(1)
         psp.dilatation_Y(1)
 
-    <+Définition des objets+>
+    triangles=[]
+    triangles.append(  Polygon(  Point(0,0),Point(1,0),Point(2,-2)  )  )
+    triangles.append(  Polygon(  Point(0,0),Point(1,1),Point(2,-1)  )  )
+    triangles.append(  Polygon(  Point(0,0),Point(0.3,1),Point(1.5,-1)  )  )
+    triangles.append(  Polygon(  Point(0,0),Point(1,1),Point(1,-2)  )  )
+    triangles.append(  Polygon(  Point(0,0),Point(1,2),Point(2,0)  )  )
 
-    for psp in pspicts:
-        psp.DrawDefaultAxes()
+    triangles[0].put_mark(0.2,["\( Q\)","\( C\)","\( M\)"],pspict=pspicts)
+    triangles[1].put_mark(0.2,["\( T\)","\( A\)","\( K\)"],pspict=pspicts)
+    triangles[2].put_mark(0.2,["\( R\)","\( N\)","\( T\)"],pspict=pspicts)
+    triangles[3].put_mark(0.2,["\( B\)","\( D\)","\( O\)"],pspict=pspicts)
+    triangles[4].put_mark(0.2,["\( F\)","\( E\)","\( H\)"],pspict=pspicts)
 
-    fig.conclude()
-    fig.write_the_file()
+    #n=1,d=0.1,l=0.1,angle=45,pspict=None):
+    med1=triangles[0].edges[0].bisector(code=(1,0.1,0.1,45,pspicts))
+    
+    pspicts[0].DrawGraphs(med1)
+
+    for i,pspict in enumerate(pspicts):
+        pspict.DrawGraphs(triangles[i])
+    
+    for fig in figs :
+        fig.no_figure()
+        fig.conclude()
+        fig.write_the_file()
