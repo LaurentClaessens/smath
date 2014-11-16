9f8549f239e8ce9be03d8c1fd8c1df0d16e14806
Laurent Claessens <moky.math@gmail.com>
2014-11-16 11:53:19 +0100

    proportionnalité 4ième

--- phystricksFLXBooMvWhiY.py
+++ phystricksFLXBooMvWhiY.py
@@ -7,15 +7,21 @@ def FLXBooMvWhiY():
     pspicts,figs = IndependentPictures("FLXBooMvWhiY",3)
 
     for psp in pspicts:
-        psp.dilatation_X(0.5)
-        psp.dilatation_Y(1)
+        psp.dilatation_X(0.3)
+        psp.dilatation_Y(0.3)
 
     pspicts[2].dilatation_Y(0.1)
 
-    pts=[   [Point(x,x/2) for x in [1,2,4,7]], [Point(x,x/2+1) for x in [1,2,4,7]], [Point(x,x**2-1) for x in [1,2,4,7]]  ]
+    x=var('x')
+    absisses=[1,2,4,7]
+    F=[   phyFunction(x/2),phyFunction(x/2+1),phyFunction(x**2-1)   ]
 
-    for i in range(len(pts)):
-        pspicts[i].DrawGraphs(pts[i])
+    pts=[   [Point(x,f(x)) for x in absisses  ] for f in F]
+
+    for i,pt in enumerate(pts) :
+        f=F[i].graph(absisses[0]-0.5,absisses[-1]+0.3)
+        f.parameters.style="dashed"
+        pspicts[i].DrawGraphs(pts[i],f)
 
     for psp in pspicts:
         psp.axes.no_graduation()
