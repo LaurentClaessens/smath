a7615e8be2460275f48584a2cfe58cf65809ebdf
Laurent Claessens <moky.math@gmail.com>
2014-11-17 14:08:30 +0100

    Encore de l'ordre dans les droites remarquables dans un triangle.

--- phystricksGARYooJCnpFS.py
+++ phystricksGARYooJCnpFS.py
@@ -4,25 +4,32 @@ import string
 
 from phystricks import *
 def GARYooJCnpFS():
-    pspict,fig = SinglePicture("GARYooJCnpFS")
-    pspict.dilatation(1)
+    pspicts,figs = IndependentPictures("GARYooJCnpFS",2)
+    for psp in pspicts: 
+        psp.dilatation(1)
 
     A=Point(0,0)
     B=Point(4,1)
     C=Point(3,3)
 
     triangle=Polygon(A,B,C)
-    triangle.put_mark(0.2,pspict=pspict)
+    triangle.put_mark(0.2,pspict=pspicts)
+    for psp in pspicts :
+        psp.DrawGraphs(triangle)
     
     for i in [0,1,2]:
+        cot=triangle.edges[ (i+1)%3 ].copy()
         P=triangle.vertices[i]
-        cot=triangle.edges[ (i+1)%3 ]
         M=cot.midpoint()
         med=Segment(M,P).dilatation(1.5)
-        cot.divide_in_two(n=i+1,d=0.1,l=0.3,pspict=pspict)
-        pspict.DrawGraph(med)
+        med.parameters.style="dashed"
 
-    pspict.DrawGraphs(triangle)
-    fig.no_figure()
-    fig.conclude()
-    fig.write_the_file()
+        pspicts[0].DrawGraphs(med)
+
+        cot.divide_in_two(n=i+1,d=0.1,l=0.3,pspict=pspicts)
+        pspicts[1].DrawGraphs(cot,med)
+
+    for fig in figs:
+        fig.no_figure()
+        fig.conclude()
+        fig.write_the_file()
--- phystricksQZPMooIiOQpy.py
+++ phystricksQZPMooIiOQpy.py
@@ -1,24 +1,29 @@
 # -*- coding: utf8 -*-
 from phystricks import *
 def QZPMooIiOQpy():
-    pspict,fig = SinglePicture("QZPMooIiOQpy")
-    pspict.dilatation(1)
+    pspicts,figs = IndependentPictures("QZPMooIiOQpy",2)
+    for psp in pspicts :
+        psp.dilatation(1)
 
     A=Point(0,0)
     B=Point(4,1)
     C=Point(2.5,3)
 
     triangle=Polygon(A,B,C)
-    triangle.put_mark(0.2,pspict=pspict)
+    triangle.put_mark(0.2,pspict=pspicts)
+    for psp in pspicts:
+        psp.DrawGraphs(triangle)
     
     for i in [0,1,2]:
         P=triangle.vertices[i]
         cot=triangle.edges[ (i+1)%3 ]
         hauteur=cot.orthogonal_trough(P).dilatation(1.5)
+        hauteur.parameters.style="dashed"
         rh=RightAngle(hauteur,cot,0.2,0,0)
-        pspict.DrawGraphs(hauteur,rh)
+        pspicts[0].DrawGraphs(hauteur)
+        pspicts[1].DrawGraphs(hauteur,rh)
 
-    pspict.DrawGraphs(triangle)
-    fig.no_figure()
-    fig.conclude()
-    fig.write_the_file()
+    for fig in figs:
+        fig.no_figure()
+        fig.conclude()
+        fig.write_the_file()
