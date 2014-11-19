946a7542f8158a26b89b8033c151ad3795f4a762
Laurent Claessens <moky.math@gmail.com>
2014-11-19 14:13:18 +0100

    Encore quelque figures et pdf

---
+++ phystricksXCWLooVgowie.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def XCWLooVgowie():
+    pspict,fig = SinglePicture("XCWLooVgowie")
+    pspict.dilatation(0.3)
+
+    A=Point(0,0)
+    B=Point(4,0)
+    C=Point(0,3)
+
+    A.put_mark(0.2,180+45,"\( A\)",automatic_place=(pspict,"corner"))
+    B.put_mark(0.2,-45,"\( B\)",automatic_place=(pspict,"corner"))
+    C.put_mark(0.2,90+45,"\( C\)",automatic_place=(pspict,"corner"))
+
+    rh=RightAngle(  Segment(C,A),Segment(A,B),0.6,0,1  )
+
+    trig=Polygon(A,B,C)
+    trig.edges[0].put_mark(0.2,-90,"\( 12\)",automatic_place=(pspict,"N"))
+
+    trig.edges[1].put_mark(0.2,trig.edges[1].advised_mark_angle*degree+180,"\( ?\)",automatic_place=(pspict,"corner"))
+    trig.edges[2].put_mark(0.2,180,"\( 9\)",automatic_place=(pspict,"E"))
+
+    pspict.DrawGraphs(A,B,C,trig,rh)
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
