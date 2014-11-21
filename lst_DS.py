5c3d060e219e35ffa2fb90e25bd149c6eed859e1
Laurent Claessens <moky.math@gmail.com>
2014-11-21 12:27:41 +0100

    DS 4A

--- lst_DS.py
+++ lst_DS.py
@@ -12,11 +12,16 @@ groupe_5A=enseignement.groupe_5A
 groupe_5B=enseignement.groupe_5B
 groupe_5AB=groupe_5A.extend(groupe_5B,"groupe_5AB")
 
+
+# 5B : Damien
+
+
 DS1_5B=commun.TheDS("DS1_5B",groupe_5B,1)
 DS2_5AB=commun.TheDS("DS2_5AB",groupe_5AB,4)
 DS2_4A=commun.TheDS("DS2_4A",groupe_4A,1)
+DS_4A3=commun.TheDS("DS_4A3",groupe_4A,1)
 
-jeveux=DS2_5AB
+jeveux=DS_4A3
 jeveux.write_the_file()
 
 myRequest = LaTeXparser.PytexTools.Request("seconde")
