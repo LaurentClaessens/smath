d459702b4117d7c238112eeabac9d515bacf2201
Laurent Claessens <moky.math@gmail.com>
2014-11-24 12:27:06 +0100

    DS3 des cinquièmes

--- lst_DS.py
+++ lst_DS.py
@@ -13,7 +13,9 @@ groupe_5B=enseignement.groupe_5B
 groupe_5AB=groupe_5A.extend(groupe_5B,"groupe_5AB")
 
 
+# Les PAI qu'il faut plus grand:
 # 5B : Damien
+# 5A : Clothilde
 
 
 DS1_5B=commun.TheDS("DS1_5B",groupe_5B,1)
@@ -23,7 +25,7 @@ DS_4A3=commun.TheDS("DS_4A3",groupe_4A,1)
 DS_5A3=commun.TheDS("DS_5A3",groupe_5A,1)
 DS_5B3=commun.TheDS("DS_5B3",groupe_5B,1)
 
-jeveux=DS_5B3
+jeveux=DS_5A3
 jeveux.write_the_file()
 
 myRequest = LaTeXparser.PytexTools.Request("seconde")
