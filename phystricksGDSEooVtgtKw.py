6f01ce14e05290ba8a40c30683b041901b84a409
Laurent Claessens <moky.math@gmail.com>
2015-01-02 00:29:46 +0100

    ... nombres relatifs...

---
+++ phystricksGDSEooVtgtKw.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def GDSEooVtgtKw():
+    pspict,fig = SinglePicture("GDSEooVtgtKw")
+    pspict.dilatation(0.6)
+
+    A=Point(3,2)
+    B=Point(-3,4)
+    C=Point(0,-4)
+    H=Point(-6.5,-2)
+
+    A.put_mark(0.2,A.angle(),"\( A\)",automatic_place=(pspict,"corner"))
+    B.put_mark(0.2,B.angle(),"\( B\)",automatic_place=(pspict,"corner"))
+    C.put_mark(0.2,C.angle(),"\( C\)",automatic_place=(pspict,"corner"))
+    H.put_mark(0.2,H.angle(),"\( H\)",automatic_place=(pspict,"S"))
+
+    pspict.DrawGraphs(A,B,C,H)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
--- phystricksQBDMooGskfKN.py
+++ phystricksQBDMooGskfKN.py
@@ -3,11 +3,29 @@ from phystricks import *
 
 def QBDMooGskfKN():
     pspict,fig = SinglePicture("QBDMooGskfKN")
-    pspict.dilatation_X(1)
-    pspict.dilatation_Y(1)
+    pspict.dilatation(0.7)
 
+    A=Point(3,2)
+    B=Point(-5,3)
+    C=Point(1,-4)
+    H=Point(-3.5,0)
 
-    pspict.DrawGraphs(<++>)
+    A.put_mark(0.2,45,"\( A\)",automatic_place=(pspict,"corner"))
+    B.put_mark(0.2,90+45,"\( B\)",automatic_place=(pspict,"corner"))
+    C.put_mark(0.2,-45,"\( C\)",automatic_place=(pspict,"corner"))
+    H.put_mark(0.2,90,"\( H\)",automatic_place=(pspict,"S"))
+
+    for P in [A,B,C]:
+        qx=Point(P.x,0)
+        qy=Point(0,P.y)
+        s1=Segment(P,qx)
+        s2=Segment(P,qy)
+        s1.parameters.style="dashed"
+        s2.parameters.style="dashed"
+        pspict.DrawGraphs(s1,s2)
+
+    pspict.DrawGraphs(A,B,C,H)
     pspict.DrawDefaultAxes()
+    fig.no_figure()
     fig.conclude()
     fig.write_the_file()
