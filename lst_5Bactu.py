d7a85f9979092473aee78012d4d66ebeb2b3879c
Laurent Claessens <moky.math@gmail.com>
2015-01-15 07:04:43 +0100

    Préparation "nombres relatifs" seconde main

--- lst_5Bactu.py
+++ lst_5Bactu.py
@@ -19,12 +19,14 @@ ops_frac=commun.OneChapter("Opérations sur les fractions",classe)
 sym_centrale=commun.OneChapter("Symétrie centrale",classe)
 exp_litt=commun.OneChapter("Expressions littérales",classe)
 ang_parall=commun.OneChapter("Angles et parallélisme",classe)
+nombres_relatifs=commun.OneChapter("Nombres relatifs",classe)
 
 
 actu5=sym_centrale
 actu5=ang_parall
-actu5=exp_litt
 actu5=ops_frac
+actu5=exp_litt
+actu5=nombres_relatifs
 actu5.set_filename=set_filename
 actu5.write_the_file()
 
