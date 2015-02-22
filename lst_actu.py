f53f1a562bb6b9464e6b506f5c41bce3306cb638
Laurent Claessens <moky.math@gmail.com>
2015-02-22 21:58:52 +0100

    ... la compilation desdites figures.

--- lst_actu.py
+++ lst_actu.py
@@ -8,9 +8,8 @@ myRequest = LaTeXparser.PytexTools.Request("seconde")
 myRequest.original_filename="smath.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
-myRequest.ok_filenames_list.append("24_autrecinquieme")
-myRequest.ok_filenames_list.append("<++>")
-myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("15_evaluation")
+myRequest.ok_filenames_list.append("16_evaluation")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
