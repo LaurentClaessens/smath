f7ec3aaa3e21a3589f4d6f1d114037de03b3542f
Laurent Claessens <moky.math@gmail.com>
2015-03-06 14:49:12 +0100

    Un exercice avec une fleur

--- phystricksCYLGooSGnRii.py
+++ phystricksCYLGooSGnRii.py
@@ -10,15 +10,16 @@ def CYLGooSGnRii():
     cone_height=7
     cylinder_radius=2
     cone_radius=3
-    cylinder_height=cone_height-(cone_height*cylinder_radius)/cone_radius   # WW Thalès !
+    left_height=(cone_height*cylinder_radius)/cone_radius   # WW Thalès !           this is the height left for the flower.
+    cylinder_height=cone_height-left_height
 
     perspective=ObliqueProjection(30,0.5)
     base_circle=Circle3D(perspective, (0,0,0),(cylinder_radius,0,0),(0,0,cylinder_radius) )
     up_circle=Circle3D(perspective, (0,cylinder_height,0),(cylinder_radius,cylinder_height,0),(0,cylinder_height,cylinder_radius) )
     cone_circle=Circle3D(perspective, (0,0,0),(cone_radius,0,0),(0,0,cone_radius) )
-    base_circle.parameters.plotpoints=100
-    up_circle.parameters.plotpoints=100
-    cone_circle.parameters.plotpoints=100
+    base_circle.parameters.plotpoints=30
+    up_circle.parameters.plotpoints=30
+    cone_circle.parameters.plotpoints=30
     base_circle.parameters.style="dashed"
     cone_circle.divide=True
     up_circle.divide=True
@@ -42,14 +43,20 @@ def CYLGooSGnRii():
     D=Point(0,cylinder_height)
     E=Point(cylinder_radius,cylinder_height)
 
-    segs=[]
-    segs.append(Segment(A,B))
-    segs.append(Segment(A,C))
-    segs.append(Segment(D,E))
-    segs.append(Segment(C,B))
+    F=perspective.point(0,cylinder_height,0)
+    photo_size=(2*left_height/3.5)*pspict.yunit       # La photo de la fleur fera les trois demi de la partie restante
+    #F.put_mark(photo_size/2-0.2,angle=90,text="\includegraphics[height="+str(photo_size)+"cm]{fp081206-28.pdf}",automatic_place=(pspict,""))
+    F.put_mark(0.01,angle=90,text="\includegraphics[height="+str(photo_size)+"cm]{fp081206-28.pdf}",automatic_place=(pspict,""))
+    
+
+    #segs=[]
+    #segs.append(Segment(A,B))
+    #segs.append(Segment(A,C))
+    #segs.append(Segment(D,E))
+    #segs.append(Segment(C,B))
 
     pspict.comment="The dashed part is not correct because of 'GraphOfACircle3D.specific_action_on_pspict' which is not precise enough when 'divide' is 'True'"
-    pspict.DrawGraphs(base_circle,up_circle,cone_circle,SK,SL,h1,h2)
+    pspict.DrawGraphs(base_circle,up_circle,cone_circle,SK,SL,h1,h2,F)
     fig.no_figure()
     fig.conclude()
     fig.write_the_file()
