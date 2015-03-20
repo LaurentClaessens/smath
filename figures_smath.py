5dde53407758914fa5396fbcc26d689de3055834
Laurent Claessens <moky.math@gmail.com>
2015-03-20 06:29:38 +0100

    encore à propos de feuille de secours 2

--- figures_smath.py
+++ figures_smath.py
@@ -8,6 +8,7 @@ Comment tester et recompiler ? Réponse dans 'recompilation.txt'
 from phystricks import *
 import sys
 
+from phystricksMWFBooZHANAt import MWFBooZHANAt
 from phystricksHRIVooBXxZES import HRIVooBXxZES
 from phystricksPIMUooMLLYDU import PIMUooMLLYDU
 from phystricksFSQMooFDOrkR import FSQMooFDOrkR
@@ -956,8 +957,8 @@ figures_list.append(KYVAooVmGYhS)
 figures_list.append(FSQMooFDOrkR)
 figures_list.append(PIMUooMLLYDU)
 figures_list.append(HRIVooBXxZES)
+figures_list.append(MWFBooZHANAt)
 """
-figures_list.append(<++>)
 figures_list.append(<++>)
 figures_list.append(<++>)
 figures_list.append(<++>)
---
+++ phystricksMWFBooZHANAt.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def MWFBooZHANAt():
+    pspict,fig = SinglePicture("MWFBooZHANAt")
+    pspict.dilatation_X(0.56)
+    pspict.dilatation_Y(0.33)
+    
+    L=10
+    l=4
+    h=3
+    A=Point(0,0)
+    B=A+(l,0)
+    C=A+(L,0)
+    D=C+(0,h)
+    E=B+(0,h)
+    F=A+(0,h)
+
+    jardin=Polygon(A,B,E,F)
+    piscine=Polygon(B,C,D,E)
+    piscine.parameters.hatched()
+    piscine.parameters.hatch.color="lightgray"
+
+    mesL=Segment(F,D).get_measure(-0.3,0.1,90,"\( 10\)",automatic_place=(pspict,"S"))
+    mesl=Segment(A,B).get_measure(0.3,0.1,-90,"\( x\)",automatic_place=(pspict,"N"))
+    mesH=Segment(A,F).get_measure(-0.3,0.1,180,"\( 3\)",automatic_place=(pspict,"E"))
+    mesP=Segment(B,C).get_measure(0.2,0.1,-90,"?",automatic_place=(pspict,"N"))
+
+    pspict.DrawGraphs(piscine,jardin,mesL,mesl,mesH,mesP)
+    #pspict.DrawGraphs(piscine,jardin,mesL,mesl,mesH,mesP)
+
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
