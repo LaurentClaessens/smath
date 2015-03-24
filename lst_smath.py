51acd012a4436a802a6cea7e3d25c0610be4fbfd
Laurent Claessens <moky.math@gmail.com>
2015-03-24 11:55:15 +0100

    Pourquoi n'y avait-il plus ?

---
+++ lst_smath.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
+import sys
+import LaTeXparser
+import LaTeXparser.PytexTools
+import commun
+
+myRequest = LaTeXparser.PytexTools.Request("seconde")
+myRequest.original_filename="smath.tex"
+
+def set_corrPosition_and_Draft(A):
+    u="\corrPosition{1}"
+    v="\corrPosition{2}"
+    A = A.replace(u, v)
+    u="\corrPosition{2}"
+    A = A.replace(u, v)
+    u="\corrDraft"
+    A=A.replace(u,"%")
+    return A
+
+def accept_all_input(medicament):
+    medicament.accept_input=lambda x: True
+
+
+myRequest.add_plugin(accept_all_input,"medicament")
+myRequest.add_plugin(set_corrPosition_and_Draft,"after_pytex")
+myRequest.add_plugin(commun.set_filename("0-smath.pdf"),"medicament")
+
+
+myRequest.original_filename="smath.tex"
