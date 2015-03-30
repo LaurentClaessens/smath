60a14ffce2db78e34f7cdee2d8f04f494d090252
Laurent Claessens <moky.math@gmail.com>
2015-03-30 12:27:31 +0200

    Exercices pour les opérations sur les nombres relatifs.

---
+++ phystricksJSYRooLLggNJ.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+
+def unexo(a,b,c,noms,pspicts,n):
+    A=Point(a,0)
+    B=Point(b,0)
+    C=Point(c,0)
+    O=Point(0,0)
+    mx=min([a-1,b-1,c-1,-2])
+    Mx=max([a+1,b+1,c+1,2])
+    axe=SingleAxe(O,Vector(1,0),mx,Mx,pspict=pspicts)
+    axe.no_numbering()
+    A.put_mark(0.2,angle=90,added_angle=0,text="\( {}\)".format(noms[0]),automatic_place=(pspicts,""))
+    B.put_mark(0.2,angle=90,added_angle=0,text="\(  {}\)".format(noms[1]),automatic_place=(pspicts,""))
+    C.put_mark(0.2,angle=90,added_angle=0,text="\( {}\)".format(noms[2]),automatic_place=(pspicts,""))
+    O.put_mark(0.2,angle=-90,added_angle=0,text="\( 0\)",automatic_place=(pspicts,""))
+    pspicts[n].DrawGraphs(A,B,C,O,axe)
+
+def JSYRooLLggNJ():
+    pspicts,figs = IndependentPictures("JSYRooLLggNJ",4)
+
+    unexo(2,3,5,"ABC",pspicts,0)
+    unexo(-2,3,-5,"KLM",pspicts,1)
+    unexo(0,3,-3,"STU",pspicts,2)
+    unexo(-2,-3,1,"DEF",pspicts,3)
+
+    for psp in pspicts:
+        psp.dilatation_X(1)
+        psp.dilatation_Y(1)
+
+    for fig in figs:
+        fig.no_figure()
+        fig.conclude()
+        fig.write_the_file()
