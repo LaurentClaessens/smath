361830dceaa9aa65fb5523f7185638800eb9ff28
Laurent Claessens <moky.math@gmail.com>
2015-04-21 10:22:56 +0200

    encore du rituel

--- lst_rituel.py
+++ lst_rituel.py
@@ -15,4 +15,4 @@ myRequest.ok_filenames_list.append("40_rituel5")
 myRequest.ok_filenames_list.append("41_rituel4")
 myRequest.ok_filenames_list.append("<++>")
 
-myRequest.add_plugin(commun.set_filename("0-actu.pdf"),"medicament")
+myRequest.add_plugin(commun.set_filename("0-rituel.pdf"),"medicament")
