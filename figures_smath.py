7dc73c43592bfe31c5027cf71c67469b894a2082
Laurent Claessens <moky.math@gmail.com>
2015-04-23 10:39:20 +0200

    Une activité mentale

--- figures_smath.py
+++ figures_smath.py
@@ -8,6 +8,7 @@ Comment tester et recompiler ? Réponse dans 'recompilation.txt'
 from phystricks import *
 import sys
 
+from phystricksXTGXooDVcEZs import XTGXooDVcEZs
 from phystricksYDTGooZGkkei import YDTGooZGkkei
 from phystricksSWYDooBCeFJb import SWYDooBCeFJb
 from phystricksXABSooLSRHxF import XABSooLSRHxF
@@ -1013,8 +1014,8 @@ figures_list.append(YLPFooXzdaFr)
 figures_list.append(XABSooLSRHxF)
 figures_list.append(SWYDooBCeFJb)
 figures_list.append(YDTGooZGkkei)
+figures_list.append(XTGXooDVcEZs)
 """
-figures_list.append(<++>)
 figures_list.append(<++>)
 figures_list.append(<++>)
 figures_list.append(<++>)
---
+++ phystricksXTGXooDVcEZs.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+from phystricks import *
+def XTGXooDVcEZs():
+    pspict,fig = SinglePicture("XTGXooDVcEZs")
+    pspict.dilatation(1.4)
+
+    C=Point(1,1)
+    A=C+(4,0)
+    B=CircleAB(C,A).get_point(66)
+    S=Segment(C,A).midpoint()
+
+    trig=Polygon(C,A,B)
+    trig.put_mark(0.2,points_names="CAB",pspict=pspict)
+    S.put_mark(0.2,angle=-90,added_angle=0,text="\( S\)",automatic_place=(pspict,""))
+
+    rh=RightAngleAOB(C,B,A)
+
+    no_symbol(trig.vertices)
+
+    pspict.DrawGraphs(trig,rh,S)
+    pspict.DrawDefaultAxes()
+    pspict.DrawDefaultGrid()
+    fig.no_figure()
+    fig.conclude()
+    fig.write_the_file()
