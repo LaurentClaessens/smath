9b886c6ba928355477089baabddcdc90e7d2092d
Laurent Claessens <moky.math@gmail.com>
2015-05-17 06:06:39 +0200

    Statistiques pour le devoir DS_5B7

--- phystricksCommons.py
+++ phystricksCommons.py
@@ -1,3 +1,5 @@
+from phystricks import *
+
 def DS_statistics(moustaches,pspict):
     pspict.dilatation_X(0.8)
     pspict.dilatation_Y(1)
@@ -9,7 +11,13 @@ def DS_statistics(moustaches,pspict):
         m.delta_y=len(moustaches)+0.75-i-1
         m.put_mark(0.2,0,"DS {}".format( i+1 ),automatic_place=(pspict,"W"))
 
-    pspict.DrawGraphs(moustaches)
+    maxy=len(moustaches)
+    ledix=Segment(  Point(10,0),Point(10,maxy)   )
+    ledix.parameters.color="green"
+    ledix.parameters.linewidth=2
+    ledix.parameters.style="dashed"
+
+    pspict.DrawGraphs(moustaches,ledix)
     pspict.grid.draw_horizontal_grid=False
     pspict.axes.draw_single_axeY=False
     pspict.axes.single_axeY.no_graduation()
