4615da0d625adeffa80a96f609eb1e0b755129fe
Laurent Claessens <moky.math@gmail.com>
2015-05-27 09:15:13 +0200

    un peu

--- lst_rituel.py
+++ lst_rituel.py
@@ -9,10 +9,10 @@ myRequest = LaTeXparser.PytexTools.Request("seconde")
 myRequest.original_filename="rituel.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
-myRequest.ok_filenames_list.append("<++>")
-myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("40_rituel5")
 myRequest.ok_filenames_list.append("41_rituel4")
+myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
 
 myRequest.add_plugin(commun.set_filename("0-rituel.pdf"),"medicament")
