57985fb63a4cea6e5bf686c0373bbeb4f6e7c8f3
Laurent Claessens <moky.math@gmail.com>
2015-06-11 11:27:47 +0200

    diagrammes en boites des DS

--- lst_actu.py
+++ lst_actu.py
@@ -6,10 +6,10 @@ import LaTeXparser
 import LaTeXparser.PytexTools
 
 myRequest = LaTeXparser.PytexTools.Request("seconde")
-myRequest.original_filename="corrDS5B8.tex"
+myRequest.original_filename="smath.tex"
 
 myRequest.ok_filenames_list=["e_smath"]
-myRequest.ok_filenames_list.append("<++>")
+myRequest.ok_filenames_list.append("49_devoirs4")
 myRequest.ok_filenames_list.append("<++>")
 myRequest.ok_filenames_list.append("<++>")
 
