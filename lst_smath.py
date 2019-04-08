7536f5a7762fcedac933625cfbd14f167299701f
Laurent Claessens <laurent@claessens-donadello.eu>
2019-04-08 21:04:06 +0200

    Sauvetage.
    - wikipédia : refaire un \newcommand qui fait presque rien de ce que
    faisait avant la fonction dans exocorr
    - pytex : mise à jour

--- lst_smath.py
+++ lst_smath.py
@@ -1,14 +1,7 @@
-#! /usr/bin/python
-# -*- coding: utf8 -*-
-
-from __future__ import unicode_literals
-
 import sys
-import latexparser
-import latexparser.PytexTools
-import commun
+from pytex.src import PytexTools
 
-myRequest = latexparser.PytexTools.Request("seconde")
+myRequest = PytexTools.Request("seconde")
 myRequest.original_filename="smath.tex"
 
 def set_corrPosition_and_Draft(A):
@@ -24,6 +17,6 @@ def set_corrPosition_and_Draft(A):
 def accept_all_input(medicament):
     medicament.accept_input=lambda x: True
 
-myRequest.add_plugin(accept_all_input,"medicament")
+myRequest.add_plugin(accept_all_input,"options")
 myRequest.add_plugin(set_corrPosition_and_Draft,"after_pytex")
 myRequest.new_output_filename="0-smath.pdf"
